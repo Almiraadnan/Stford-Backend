d935a2d8eaebade1465f4a557368b5fe649f54d1
Almiraadnan <almiraadnan34@gmail.com>
2024-07-04 23:46:26 -0700

    some-code

--- Controller/User.Controller.js
+++ Controller/User.Controller.js
@@ -100,8 +100,7 @@ const Logout = async (req, res, next) => {
 const getProfile = async (req, res) => {
   try {
     const token = req.body
-    const tokenData = JSON.parse(token.token)
-    var decoded = jwt.verify(tokenData, JWTSecret);
+    var decoded = jwt.verify(token, JWTSecret);
     const user = await UserModel.findOne({ _id: decoded.id })
     res.status(200).json({
       success: true,
